568caf5ed6d749fc588adf7589086f1f29272550
jay-so <merry9504@naver.com>
2024-05-02 18:30:17 +0900

    feat:정렬 예제 풀이 추가

---
+++ src/main/java/com/algorithm/sort/select/Main.java
@@ -0,0 +1,28 @@
+package com.algorithm.sort.select;
+
+public class Main {
+    public static void main(String[] args) {
+        //1.배열 초기화
+
+        int n = 10;
+        int[] arr = {7, 5, 9, 0, 3, 1, 6, 2, 4, 8};
+
+        for (int i = 0; i < n; i++) {
+            //2.가장 작은 원소의 인덱스
+            int min_index = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[min_index] > arr[j]) {
+                    min_index = j;
+                }
+            }
+            // 3.스와프
+            int temp = arr[i];
+            arr[i] = arr[min_index];
+            arr[min_index] = temp;
+        }
+
+        for(int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
